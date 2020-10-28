5ab28ea3389590b46581e196332096a1fce776c2
ORELLL <orlikszym@gmail.com>
2020-10-28 20:29:36 +0100

    lekcja 4

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("hello start");
+        Debug.Log("hello start");
     }
 
     // Update is called once per frame
