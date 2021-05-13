8455a86001cc8b6631f37ec8624abd549b558eb9
ORELLL <orlikszym@gmail.com>
2021-05-13 19:42:06 +0200

    połowa lekcji 30

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -1,8 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    //Play Game
+    public void PlayGame(){
+        Debug.Log("Starting game");
+        var currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+
+    //Settings
+
+    //Exit
+    public void MoveToExit(){
+        
+    }
 
 }
